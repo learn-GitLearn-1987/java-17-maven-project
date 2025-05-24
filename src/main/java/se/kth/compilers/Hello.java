c50be8f736ed1586ef08ebc58615c00052dcf837
learn-GitLearn-1987 <sonkar.anuragmanis@gmail.com>
2025-05-24 13:33:01 +0530

    fixed

--- src/main/java/se/kth/compilers/Hello.java
+++ src/main/java/se/kth/compilers/Hello.java
@@ -3,9 +3,9 @@ package se.kth.compilers;
 public class Hello {
   public static void main(String[] args) {
     if (args.length > 0) {
-      System.out.printf("Hello1234 %s!", args[0]);
+      System.out.printf("Hello %s!", args[0]);
       return;
     }
-    System.out.print("Hello World! 1234");
+    System.out.print("Hello World!");
   }
 }
